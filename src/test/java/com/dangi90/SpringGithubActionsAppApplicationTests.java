96007244d48ac448a494d63bcaa4036567bfe5ec
Dangi90 <woo24465@gmail.com>
2024-09-19 15:30:39 +0900

    actions

---
+++ src/test/java/com/dangi90/SpringGithubActionsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dangi90;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGithubActionsAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
